7eb422f451ba2b1df430b1887806421ecd0b0d02
HCW <jingi6052@naver.com>
2022-07-08 15:28:04 +0900

    buyerInfo to modal

---
+++ src/js/app.js
@@ -0,0 +1,61 @@
+App = {
+  web3Provider: null,
+  contracts: {},
+	
+  init: function() {
+   $.getJSON('../real-estate.json', function(data){
+    var list = $('#list');
+    var template =$('#template');
+
+    for(i = 0; i < data.length; i++){
+      template.find('img').attr('src', data[i].picture);
+      template.find('id').text(data[i].id);
+      template.find('type').text(data[i].type);
+      template.find('area').text(data[i].area);
+      template.find('price').text(data[i].price);
+
+      list.append(template.html());
+    }
+   })
+
+   return App.initWeb3();
+  },
+
+  initWeb3: function() {
+    if(typeof web3 !== 'undefined'){
+      App.web3Provider = web3.currentProvider;
+      web3 = new Web3(web3.currentProvider);
+
+    } else{
+      App.web3Provider = new web3.providers.HttpProvider('http://127.0.0.1:8546');
+      web3 = new Web3(App.web3Provider);
+    }
+
+    return APp.initContract();
+  },
+
+  initContract: function() {
+    $.getJSON('RealEstate.json', function(data){
+      App.contracts.RealEstate = TruffleContract(data);
+      App.contracts.RealEstate.setProvider(App.web3Provider)
+    })
+  },
+
+  buyRealEstate: function() {	
+
+  },
+
+  loadRealEstates: function() {
+	
+  },
+	
+  listenToEvents: function() {
+	
+  }
+};
+
+$(function() {
+  $(window).load(function() {
+    App.init();
+  });
+});
